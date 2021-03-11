6be4826bc53dc066efa05e407585b41751f4546f
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-03-11 13:27:22 +0100

    Added DbContext

---
+++ AdventureWorksLT-Sample-CsApp/SampleDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorksLT_Sample_CsApp
+{
+  public class SampleDbContext : DbContext
+  {
+    public SampleDbContext(DbContextOptions<SampleDbContext> contextOptions) : base(contextOptions)
+    {
+
+    }
+  }
+}
--- AdventureWorksLT-Sample-CsApp/Startup.cs
+++ AdventureWorksLT-Sample-CsApp/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AdventureWorksLT_Sample_CsApp
 {
@@ -24,6 +20,8 @@ namespace AdventureWorksLT_Sample_CsApp
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddRazorPages();
+      services.AddDbContext<SampleDbContext>(_ => 
+        _.UseSqlServer(Configuration.GetConnectionString("Default")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
