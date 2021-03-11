d813a939b9329650f1ee28124661b81681569b35
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-03-11 14:13:42 +0100

    Added active products count

--- AdventureWorksLT-Sample-CsApp/Pages/ProductsCount.cshtml.cs
+++ AdventureWorksLT-Sample-CsApp/Pages/ProductsCount.cshtml.cs
@@ -1,24 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdventureWorksLT_Sample_CsApp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AdventureWorksLT_Sample_CsApp.Pages
 {
   public class PrivacyModel : PageModel
   {
     private readonly ILogger<PrivacyModel> _logger;
+    private readonly SampleDatabaseContext _dbContext;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger, SampleDatabaseContext dbContext)
     {
       _logger = logger;
+      _dbContext = dbContext;
     }
 
     public void OnGet()
     {
+      var count = _dbContext.Products
+        .Where(_ => _.SellEndDate == null)
+        .Count();
+
+      ViewData["count"] = count;
     }
   }
 }
