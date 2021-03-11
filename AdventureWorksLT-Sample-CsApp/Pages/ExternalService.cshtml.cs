5dbbbca0307351c99618722f8fccff5795e30600
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-03-11 14:47:06 +0100

    Added ExtrenalService

---
+++ AdventureWorksLT-Sample-CsApp/Pages/ExternalService.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+
+namespace AdventureWorksLT_Sample_CsApp.Pages
+{
+  public class Cat
+  {
+    public string Url { get; set; }
+  }
+
+  public class ExternalModel : PageModel
+  {
+    private readonly ILogger<ExternalModel> _logger;
+
+    public string ImgUrl { get; set; }
+
+    public ExternalModel(ILogger<ExternalModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet()
+    {
+      using (var wc = new WebClient())
+      {
+        var resp = wc.DownloadString("https://api.thecatapi.com/v1/images/search");
+        var data = JsonConvert.DeserializeObject<Cat[]>(resp);
+
+        ImgUrl = data.First().Url;
+      }
+    }
+  }
+}
